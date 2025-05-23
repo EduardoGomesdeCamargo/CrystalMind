c44c7c575fb516e9df2739471d88c2f332db4505
EduardoGomesdeCamargo <eduardo.camargo@sptech.com>
2025-05-23 13:54:59 -0300

    criar base de dados

---
+++ src/database/CrystalMind.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE CrystalMind;
+USE CrystalMind;
+
+create table usuario (
+	id int primary key auto_increment,
+	primeiro_nome varchar(45),
+    sobrenome varchar(45),
+    email varchar(45),
+    senha varchar(45)
+);
+
+select * from usuario;
